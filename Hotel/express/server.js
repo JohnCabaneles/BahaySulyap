ac99cbc14641a60446522fd25c4e7ee56d7b2b37
unknown <andres.dev1105@gmail.com>
2024-01-25 14:18:40 +0800

    testing

--- Hotel/express/server.js
+++ Hotel/express/server.js
@@ -1,10 +1,7 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = 4000;
-
-// app.get('/',(req,res) => {
-//     res.send('Welcome to Bahay Sulyap');
-// });
 
 // Static Files
 app.use(express.static('public'));
@@ -12,8 +9,14 @@ app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/img', express.static(__dirname + 'public/img'))
 
+
+// Static Routes
 app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/views/index.html')
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/about', (req,res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'))
 })
 
 
