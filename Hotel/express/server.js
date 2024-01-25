290a9737b0eaaba8e37b26df2ac28d4de1baca3a
John-WD31 <j.cabs8888@gmail.com>
2024-01-25 12:12:37 +0800

    style added

--- Hotel/express/server.js
+++ Hotel/express/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 const app = express();
 const port = 4000;
@@ -6,15 +7,19 @@ const port = 4000;
 //     res.send('Welcome to Bahay Sulyap');
 // });
 
-// Static Files
+//middleware & static files
 app.use(express.static('public'));
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/img', express.static(__dirname + 'public/img'))
 
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/views/index.html')
+// routes
+app.get('/home', (req,res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
+
+app.get('/about', (req,res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
+
 
 
 app.listen(port, () => console.info(`Listening on port ${port}`))
